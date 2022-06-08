81f88086a80e430b0e341bc8c9bc0a7ed68cdbda
Haervwe <juan-jose-007@hotmail.com>
2022-06-08 17:19:08 -0300

    completes exervise 05 - js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let result = 0;
+    let lNum = 0;
+    let hNum = 0;
+    
+    if ( a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number"){
+        return "ERROR"
+    }
 
+    if (a < b){
+        lNum = a;
+        hNum = b;
+    } else {
+        lNum = b;
+        hNum = a;
+    }
+    
+
+    for (lNum ; lNum <= hNum; lNum++){
+        result += lNum;
+    } 
+    return result;
 };
 
 // Do not edit below this line
