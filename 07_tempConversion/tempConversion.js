81bf6c2706040a90e159afa458f29caa7e06813d
Haervwe <juan-jose-007@hotmail.com>
2022-06-08 17:41:05 -0300

    completes exervise 07 - js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let c = 0;
+  c =  (temp - 32) / ( 9 / 5);
+  return Math.round(c * 10) / 10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let f = 0;
+  f = (temp * 9 / 5 ) + 32 ;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
