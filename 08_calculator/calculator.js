b9a6f98073a0739cf0efabd198e41b389245be01
Haervwe <juan-jose-007@hotmail.com>
2022-06-13 01:38:31 -0300

    completes exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let b  = 0
+  a.forEach((a)=>Math.floor(b+=a));
+  return b;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let b  = 1
+  a.forEach((a)=>Math.floor(b*=a));
+  return b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a==0){
+    return 1;
+  }
+  let b = 1;
+  for (let i= 1; i<=a; i++){
+    b = b*i;
+  }
+  return b;
+
 };
 
 // Do not edit below this line
