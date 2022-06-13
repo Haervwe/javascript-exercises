8a6d1ebf1582241c7993f59b6bb3933b0c1e8e8c
Haervwe <juan-jose-007@hotmail.com>
2022-06-13 02:22:05 -0300

    completes exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let x = +n;
+    
+    num = Math.floor(x);
+    if (num < 0){
+        return "OOPS";
+    }
+    
+    let a = 1;
+    let b = 1;
+    let c = 0;
+    if (num<=2){
+        return 1;
+    }
+    for (let i = 3;i<=num;i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
