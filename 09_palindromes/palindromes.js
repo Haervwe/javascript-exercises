c70f07ff8fd0167b4db49eced476ffda4ea230cf
Haervwe <juan-jose-007@hotmail.com>
2022-06-13 02:03:05 -0300

    completes exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (stg) {
+let replaced = stg.toLowerCase().replace(/[^a-z]/gi, '');
+let splited = replaced.split("");
+let reversed = splited.reverse();
+let reversedStg = reversed.join("");
+console.log(reversed);
+if (replaced == reversedStg){
+    
+    console.log(reversedStg);
+    return true;
+}
+return false;
 };
 
 // Do not edit below this line
