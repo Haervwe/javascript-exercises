d1f5ad9ae0468e414a00ef9483f12ec8c7215f92
Haervwe <juan-jose-007@hotmail.com>
2022-06-13 02:50:10 -0300

    completes exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    const sorted = a.sort(function(a,b){
+        if (!b.yearOfDeath){
+            b.yearOfDeath = 2022;
+        }
+        if (!a.yearOfDeath){
+            a.yearOfDeath = 2022;
+        }
+        if ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)){
+          return 1;
+        }
+        if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
+          return -1;
+        }
+        return 0;
+      });
 
+      return sorted[0];
 };
 
 // Do not edit below this line
